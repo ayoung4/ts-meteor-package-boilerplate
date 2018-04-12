d86f0e8dbff9c30deb8af119b7ba180e72bf2841
Alfred <alfred@codenaturally.com>
2018-04-12 14:50:52 -0700

    ts compile step

--- package.js
+++ package.js
@@ -6,16 +6,25 @@ Package.describe({
     documentation: 'README.md'
 });
 
+Package.registerBuildPlugin({
+    name: 'typescript',
+    use: ['barbatus:typescript-compiler'],
+    sources: ['plugin.js']
+});
+
 Package.onUse(function (api) {
     api.versionsFrom('1.6.1.1');
-    api.use('ecmascript');
-    api.mainModule('index.js');
+    api.use('isobuild:compiler-plugin@1.0.0');
+    api.use('barbatus:typescript-compiler@0.9.12');
+    api.use('modules@0.9.2');
+    api.imply('barbatus:typescript-runtime@1.0.2');
+    api.mainModule('index.ts');
 });
 
 Package.onTest(function (api) {
-    api.use('ecmascript');
     api.use('meteortesting:mocha');
     api.use('practicalmeteor:chai');
+    api.use('modules@0.9.2');
     api.use('ayoung4:ts-meteor-package-boilerplate');
-    api.mainModule('tests.js');
+    api.mainModule('tests.ts');
 });
